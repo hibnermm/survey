40649624bf60030d6cdb8d7b4fa0818c0b1a4600
J.J. Hibner <jjmhibner@gmail.com>
2023-06-17 13:46:39 -0700

    make new branch to test out mathRandom language picker, work in progress to display language chosen in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 window.onload = function (){
   let formBtn = document.querySelector(".form-btn");
-  let name = document.querySelector("#name");
-  let randomLanguage = Math.random(formBtn);
+  let name = document.querySelector("#name").value;
+  function languagePicker(){
+    let random = Math.floor(Math.random()*10);
+    let language = "";
+    if(random < 4){
+      return language = "Python";
+    }else if (random >=4 && random <7){
+      return language = "Ruby";
+    }else{
+      return language = "Javascript";
+    }
+  };
   let output = document.querySelector("#output")
   formBtn.onclick = function(){
       output.innerHTML = ` ${name}, let's learn!`;
