bd62ce417f6ae7c779a30e59ae8bfc9765994309
J.J. Hibner <jjmhibner@gmail.com>
2023-06-17 15:56:33 -0700

    utilize Math.random to choose language, insert name and language output in HTML, and reset form

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
-window.onload = function (){
-  let formBtn = document.querySelector(".form-btn");
-  let name = document.querySelector("#name").value;
-  function languagePicker(){
-    let random = Math.floor(Math.random()*10);
-    let language = "";
-    if(random < 4){
-      return language = "Python";
-    }else if (random >=4 && random <7){
-      return language = "Ruby";
-    }else{
-      return language = "Javascript";
-    }
-  };
-  let output = document.querySelector("#output")
-  formBtn.onclick = function(){
-      output.innerHTML = ` ${name}, let's learn!`;
-  }
+window.onload = function(){
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
+  let random = Math.floor(Math.random()*10);
+  let language = languagePicker();
+  function languagePicker(random){
+      if(random < 4){
+        return "Python";
+      }else if (random >=4 && random <7){
+        return "Ruby";
+      }else{
+        return "Javascript";
+      }
+      language;
+    };
+
+    languagePicker();
+
+    let nameInput = document.querySelector("#name").value;
+    let nameCaps = nameInput.charAt(0).toUpperCase() + nameInput.slice(1);
+    console.log(nameInput);
+    let output = document.querySelector("#output");
+    output.innerHTML = `${nameCaps}, let's learn ${language}!`;
+    document.querySelector(".hidden").removeAttribute("class");
+
+    form.reset();
+  });
 };
 
 
