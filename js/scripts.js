308651edb17a625987ab87d024861244d7369f8f
J.J. Hibner <jjmhibner@gmail.com>
2023-06-17 16:55:40 -0700

    add radial choice function and test functionality for each question

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,50 @@
 window.onload = function(){
   let form = document.querySelector("form");
   form.addEventListener("submit", (event) => {
-    event.preventDefault();
+      event.preventDefault();  
+      let experZero = document.querySelector("#zero-month").checked;  
+      let experOneThree = document.querySelector("#onethree-month").checked;  
+      let experFourSix = document.querySelector("#foursix-month").checked;  
+      let experOverSix = document.querySelector("#oversix-month").checked;  
+      let yesData = document.querySelector("#yes-data").checked;
+      let noData = document.querySelector("#no-data").checked;
+      let yesBackend = document.querySelector("#yes-backend").checked;
+      let noBackend = document.querySelector("#no-backend").checked;
+      let yesFrontend = document.querySelector("#yes-frontend").checked;
+      let noFrontend = document.querySelector("#no-frontend").checked;
+      let output = document.querySelector("#output");
 
-  let random = Math.floor(Math.random()*10);
-  let language = languagePicker();
-  function languagePicker(random){
-      if(random < 4){
-        return "Python";
-      }else if (random >=4 && random <7){
-        return "Ruby";
-      }else{
-        return "Javascript";
-      }
-      language;
-    };
+      let language = radialChoice();
+      function radialChoice(){
+          if(experZero || experOneThree){
+            return output.innerHTML = "Python";
+          }else if (experFourSix){
+            return output.innerHTML = "Ruby";
+          }else if(experOverSix){
+            return output.innerHTML = "Javascript";
+          }else if(yesData){
+            return output.innerHTML = "Python";
+          }else if(noData && yesBackend || noData && noFrontend){
+            return output.innerHTML = "Ruby";
+          }else if(noData && noBackend || noData && yesFrontend){
+            return output.innerHTML = "Javascript";
+          }else{
+            return output.innerHTML = "Python";
+          };
+          language;
+          };
 
-    languagePicker();
 
-    let nameInput = document.querySelector("#name").value;
-    let nameCaps = nameInput.charAt(0).toUpperCase() + nameInput.slice(1);
-    console.log(nameInput);
-    let output = document.querySelector("#output");
-    output.innerHTML = `${nameCaps}, let's learn ${language}!`;
-    document.querySelector(".hidden").removeAttribute("class");
+        radialChoice();
 
-    form.reset();
-  });
+        let nameInput = document.querySelector("#name").value;
+        let nameCaps = nameInput.charAt(0).toUpperCase() + nameInput.slice(1);
+        console.log(nameInput);
+        output.innerHTML = `${nameCaps}, let's learn ${language}!`;
+        document.querySelector(".hidden").removeAttribute("class");
+
+        form.reset();
+      });
 };
 
 
